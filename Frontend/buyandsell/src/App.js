c0753d74752563a23060bcc9a48b540d5a35850b
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2023-04-11 23:57:58 +0200

    Change PromotionBar location

--- Frontend/buyandsell/src/App.js
+++ Frontend/buyandsell/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
-import PromotionBar from './Components/PromotionBar/PromotionBar';
 
 function App() {
   return (
     <>
-      <PromotionBar />
       <Navbar />
       <div style={{ minHeight: '100vh', backgroundColor: 'silver' }} />
       <Footer />
