1d1167f671ce5e7d48da0d3ce5f6a014a09085c1
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2023-04-12 18:04:07 +0200

    Add Categories

---
+++ Frontend/buyandsell/src/Categories/ENG/CategoriesENG.js
@@ -0,0 +1,56 @@
+export default categoriesENG = [
+    {
+        name: "Electronics",
+        subcategories: [
+            {
+                name: "Televisions and Electronics.",
+                subcategories: [
+                    { name: "Televisions", subcategories: [] },
+                    { name: "DVD/Blu-Ray Players", subcategories: [] },
+                    { name: "Speakers", subcategories: [] },
+                    { name: "Headphones", subcategories: [] },
+                    { name: "Cameras", subcategories: [] },
+                ],
+            },
+            {
+                name: "Computers & Accessories",
+                subcategories: [
+                    { name: "Laptops", subcategories: [] },
+                    { name: "Desktops", subcategories: [] },
+                    { name: "Tablets", subcategories: [] },
+                    { name: "Computer Accessories", subcategories: [] },
+                ],
+            },
+        ],
+    },
+    {
+        name: "Fashion",
+        subcategories: [
+            {
+                name: "Clothing",
+                subcategories: [
+                    { name: "Men's", subcategories: [] },
+                    { name: "Women's", subcategories: [] },
+                    { name: "Children's", subcategories: [] },
+                ],
+            },
+            {
+                name: "Footwear",
+                subcategories: [
+                    { name: "Men's", subcategories: [] },
+                    { name: "Women's", subcategories: [] },
+                    { name: "Children's", subcategories: [] },
+                ],
+            },
+            { name: "Accessories", subcategories: [] },
+        ],
+    },
+    {
+        name: "Real Estate",
+        subcategories: [
+            { name: "Apartments", subcategories: [] },
+            { name: "Houses", subcategories: [] },
+            { name: "Land plots", subcategories: [] },
+        ],
+    },
+];
---
+++ Frontend/buyandsell/src/Categories/PL/CategoriesPL.js
@@ -0,0 +1,56 @@
+export default categoriesPL = [
+    {
+        name: "Elektronika",
+        subcategories: [
+            {
+                name: "Telewizory i RTV",
+                subcategories: [
+                    { name: "Telewizory", subcategories: [] },
+                    { name: "Odtwarzacze DVD/Blu-Ray", subcategories: [] },
+                    { name: "Głośniki", subcategories: [] },
+                    { name: "Słuchawki", subcategories: [] },
+                    { name: "Kamery", subcategories: [] },
+                ],
+            },
+            {
+                name: "Komputery i akcesoria",
+                subcategories: [
+                    { name: "Laptopy", subcategories: [] },
+                    { name: "Komputery stacjonarne", subcategories: [] },
+                    { name: "Tablety", subcategories: [] },
+                    { name: "Akcesoria komputerowe", subcategories: [] },
+                ],
+            },
+        ],
+    },
+    {
+        name: "Moda",
+        subcategories: [
+            {
+                name: "Odzież",
+                subcategories: [
+                    { name: "Męska", subcategories: [] },
+                    { name: "Damska", subcategories: [] },
+                    { name: "Dziecięca", subcategories: [] },
+                ],
+            },
+            {
+                name: "Obuwie",
+                subcategories: [
+                    { name: "Męskie", subcategories: [] },
+                    { name: "Damskie", subcategories: [] },
+                    { name: "Dziecięce", subcategories: [] },
+                ],
+            },
+            { name: "Dodatki", subcategories: [] },
+        ],
+    },
+    {
+        name: "Nieruchomości",
+        subcategories: [
+            { name: "Mieszkania", subcategories: [] },
+            { name: "Domy", subcategories: [] },
+            { name: "Działki", subcategories: [] },
+        ],
+    },
+];
